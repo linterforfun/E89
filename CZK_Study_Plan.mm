76edd86728137ca4107b96ed4aa18c22684e5a6f
chenzhuokang <chenzhuokang@cvte.com>
2016-10-30 12:25:44 +0900

    not

--- CZK_Study_Plan.mm
+++ CZK_Study_Plan.mm
@@ -1,12 +1,13 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1477142611470" ID="ID_1676645327" MODIFIED="1477142623448" TEXT="&#x5b66;&#x4e60;&#x8ba1;&#x5212;">
+<node CREATED="1477142611470" ID="ID_1676645327" MODIFIED="1477797893459" TEXT="&#x5b66;&#x4e60;&#x8ba1;&#x5212;">
 <node CREATED="1477142638988" ID="ID_288057385" MODIFIED="1477142645919" POSITION="right" TEXT="2016.10.22">
 <node CREATED="1477142677010" ID="ID_377306778" MODIFIED="1477148381531" TEXT="1.fgets&#x5de5;&#x4f5c;&#x65b9;&#x5f0f;">
 <node CREATED="1477148356438" ID="ID_1889258221" MODIFIED="1477148427612" TEXT="fgets&#x6bcf;&#x6b21;&#x8bfb;&#x53d6;&#x4e00;&#x884c;&#xff0c;&#x5373;&#x8bfb;&#x5230;\n&#x7ed3;&#x675f;"/>
 </node>
 <node CREATED="1477142697698" ID="ID_834372574" MODIFIED="1477142712656" TEXT="2.&#x5b9e;&#x73b0;HTTP&#x534f;&#x8bae;&#x89e3;&#x6790;&#x7a0b;&#x5e8f;"/>
-<node CREATED="1477142723544" ID="ID_1496922009" MODIFIED="1477142730438" TEXT="3.Lua&#x5b9e;&#x73b0;&#x7ea2;&#x9ed1;&#x6811;"/>
+<node CREATED="1477142723544" ID="ID_1496922009" MODIFIED="1477797850104" TEXT="3.Javascript&#x5b9e;&#x73b0;&#x7ea2;&#x9ed1;&#x6811;"/>
 </node>
+<node CREATED="1477797905441" ID="ID_21269084" MODIFIED="1477797912251" POSITION="left" TEXT="&#x5728;&#x8ba1;&#x7b97;&#x673a;&#x79d1;&#x5b66;&#x91cc;&#xff0c;&#x5c3e;&#x8c03;&#x7528;&#x662f;&#x6307;&#x4e00;&#x4e2a;&#x51fd;&#x6570;&#x91cc;&#x7684;&#x6700;&#x540e;&#x4e00;&#x4e2a;&#x52a8;&#x4f5c;&#x662f;&#x4e00;&#x4e2a;&#x51fd;&#x6570;&#x8c03;&#x7528;&#x7684;&#x60c5;&#x5f62;&#xff1a;&#x5373;&#x8fd9;&#x4e2a;&#x8c03;&#x7528;&#x7684;&#x8fd4;&#x56de;&#x503c;&#x76f4;&#x63a5;&#x88ab;&#x5f53;&#x524d;&#x51fd;&#x6570;&#x8fd4;&#x56de;&#x7684;&#x60c5;&#x5f62;&#x3002;"/>
 </node>
 </map>
